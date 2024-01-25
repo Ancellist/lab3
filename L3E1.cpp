863e412e39780ed057713687586d5f203063ff14
Ancellist <83424461+Ancellist@users.noreply.github.com>
2024-01-25 07:35:24 +0400

    Add files via upload

---
+++ L3E1.cpp
@@ -0,0 +1,30 @@
+﻿// L3E1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include <Windows.h>
+#include <iostream>
+
+void main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int a, b, min, max;
+
+    printf("Введите первое число = ");
+    scanf_s("%d", &a);
+    printf("Введите второе число = ");
+    scanf_s("%d", &b);
+
+    if (a < b) {
+        min = a;
+        max = b;
+    }
+    else
+    {
+        max = a;
+        min = b;
+    }
+    printf("Максимальное число %d \n",max);
+    printf("Минимальное число %d \n",min);
+}
+
